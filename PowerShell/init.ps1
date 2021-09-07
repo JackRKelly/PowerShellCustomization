37047c7731aeee34c70ed17a8639104cd12d17ff
JackRKelly <kcjackkelly@gmail.com>
2021-09-06 21:55:29 -0500

    Add powershell and windows terminal comments

--- PowerShell/init.ps1
+++ PowerShell/init.ps1
@@ -1,4 +1,11 @@
+# Install powershell 7 
+# https://github.com/PowerShell/PowerShell/releases/tag/v7.1.4
+
+# Install Windows Terminal Preview
+# https://www.microsoft.com/en-us/p/windows-terminal-preview/9n8g5rfz9xk3?activetab=pivot:overviewtab
+
 Install-Module Nuget
 Install-Module posh-git
 Install-Module oh-my-posh
 Install-Module Terminal-Icons
+
